0c3021b1b90ba127483ba260083755c6b14d686f
GxStyle <82037365+GxStyle@users.noreply.github.com>
2022-09-30 18:15:10 -0300

    Login_registrar
    vamos poco a poco

---
+++ html/func/registrarse.php
@@ -0,0 +1,12 @@
+<?php
+require '../../database/database.php';
+
+if (isset($_POST['submit_register'])) {
+
+
+    echo "<script>alert('Bien');window.location= '../pages/login_register.php'</script>";
+    
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+ <?php 
+  session_start();
+
+  session_unset();
+
+  session_destroy();
+ echo "<script> window.location='html/pages/inicio.php'; </script>";
+ ?> 
+
+<!--
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="../../css/w3.css">
+    <link rel="stylesheet" href="../../css/w3-theme-black.css">
+    <link rel="stylesheet" href="../../css/main.scss">
+    <link rel="icon" href="../../assets/icon.jfif">
+    <script src="../../script/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body width=100%; max-height=100%;></body>-->
