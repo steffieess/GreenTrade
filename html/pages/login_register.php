c2d50ec9c0c2bb14f69840b7e42bc79038b0b94d
GxStyle <82037365+GxStyle@users.noreply.github.com>
2022-09-29 23:32:33 -0300

    Posible login-registro

--- html/pages/login_register.php
+++ html/pages/login_register.php
@@ -1,16 +1,38 @@
 <!-- header section starts  -->
 <?php include("../../includes/header.php"); ?>
 <!-- header section ends -->
+<style type="text/css">
+    #global {
+        width: 300px;
+        height: 300px;
+        overflow-y: scroll;
+    }
+
+    #global::-webkit-scrollbar {
+        width: 1rem;
+    }
+
+    #global::-webkit-scrollbar-track {
+        background-color: white;
+    }
+
+    #global::-webkit-scrollbar-thumb {
+        background-color: #4c7031;
+    }
+</style>
 
 <body>
 
     <div id='login-form' class='login-page'>
+
         <div class="form-box">
+
             <div class='button-box'>
                 <div id='btn'></div>
                 <button type='button' onclick='login()' class='toggle-btn'>Log In</button>
                 <button type='button' onclick='register()' class='toggle-btn'>Register</button>
             </div>
+
             <form id='login' class='input-group-login'>
                 <h1>Iniciar Sesión</h1>
                 <div class="content">
@@ -25,57 +47,63 @@
                     <button class="submit-btn" type="submit" value="submit"> <span>Ingresar</span></button>
                 </div>
             </form>
+
             <form id='register' class='input-group-register'>
-                <h1>Registrar Empresa</h1>
-                <div class="content">
-                    <label for="razon"><b>Razón social</b></label>
-                    <input name="razon" type="text" class='input-field' placeholder="Razón social" required>
+                <div id="global">
+                    <h1>Registrar Empresa</h1>
+                    <div class="content">
+                        <label for="razon"><b>Razón social</b></label>
+                        <input name="razon" type="text" class='input-field' placeholder="Razón social" required>
+                    </div>
+                    <div class="content">
+                        <label for="direc_empresa"><b>Dirección Empresa</b></label>
+                        <input name="direc_empresa" type="text" class='input-field' placeholder="Dirección Empresa" required>
+                    </div>
+                    <div class="content">
+                        <label for="tel_empresa"><b>Teléfono Empresa</b></label>
+                        <input name="tel_empresa" type="text" class='input-field' placeholder="Teléfono Empresa" required>
+                    </div>
+                    <div class="content">
+                        <label for="mail_empresa"><b>Correo Empresa</b></label>
+                        <input name="mail_empresa" type="text" class='input-field' placeholder="greentrade@greentrade.com" required>
+                    </div>
+                    <div>
+                        <button class="submit-btn" type="submit" value="submit"> <span>Ingresar</span></button>
+                    </div>
+                    <h1>Registrar Representante de la Empresa</h1>
+                    <div class="content">
+                        <label for="rut_user"><b>Rut representante</b></label>
+                        <input name="rut_user" type="text" class='input-field' placeholder="11111111-1" required>
+                    </div>
+                    <div class="content">
+                        <label for="nom_user"><b>Nombre representante</b></label>
+                        <input name="nom_user" type="text" class='input-field' placeholder="Nombre representante" required>
+                    </div>
+                    <div class="content">
+                        <label for="appat_user"><b>Apellido paterno representante</b></label>
+                        <input name="appat_user" type="text" class='input-field' placeholder="Apellido Paterno" required>
+                    </div>
+                    <div class="content">
+                        <label for="apmat_user"><b>Apellido materno representante</b></label>
+                        <input name="apmat_user" type="text" class='input-field' placeholder="Apellido Materno" required>
+                    </div>
+                    <div class="content">
+                        <label for="mail_user"><b>Correo representante</b></label>
+                        <input name="mail_user" type="text" class='input-field' placeholder="greentrade@greentrade.com" required>
+                    </div>
+                    <div class="content">
+                        <label for="tel_user"><b>Teléfono representante</b></label>
+                        <input name="tel_user" type="text" class='input-field' placeholder="Teléfono" required>
+                    </div>
+                    <div>
+                        <button class="submit-btn" type="submit" value="submit"> <span>Ingresar</span></button>
+                    </div>
                 </div>
-                <div class="content">
-                    <label for="direc_empresa"><b>Dirección Empresa</b></label>
-                    <input name="direc_empresa" type="text" class='input-field' placeholder="Dirección Empresa" required>
-                </div>
-                <div class="content">
-                    <label for="tel_empresa"><b>Teléfono Empresa</b></label>
-                    <input name="tel_empresa" type="text" class='input-field' placeholder="Teléfono Empresa" required>
-                </div>
-                <div class="content">
-                    <label for="mail_empresa"><b>Correo Empresa</b></label>
-                    <input name="mail_empresa" type="text" class='input-field' placeholder="greentrade@greentrade.com" required>
-                </div>
-                <div>
-                    <button class="submit-btn" type="submit" value="submit"> <span>Ingresar</span></button>
-                </div>
-                <h1>Registrar Representante de la Empresa</h1>
-                <div class="content">
-                    <label for="rut_user"><b>Rut representante</b></label>
-                    <input name="rut_user" type="text" class='input-field' placeholder="11111111-1" required>
-                </div>
-                <div class="content">
-                    <label for="nom_user"><b>Nombre representante</b></label>
-                    <input name="nom_user" type="text" class='input-field' placeholder="Nombre representante" required>
-                </div>
-                <div class="content">
-                    <label for="appat_user"><b>Apellido paterno representante</b></label>
-                    <input name="appat_user" type="text" class='input-field' placeholder="Apellido Paterno" required>
-                </div>
-                <div class="content">
-                    <label for="apmat_user"><b>Apellido materno representante</b></label>
-                    <input name="apmat_user" type="text" class='input-field' placeholder="Apellido Materno" required>
-                </div>
-                <div class="content">
-                    <label for="mail_user"><b>Correo representante</b></label>
-                    <input name="mail_user" type="text" class='input-field' placeholder="greentrade@greentrade.com" required>
-                </div>
-                <div class="content">
-                    <label for="tel_user"><b>Teléfono representante</b></label>
-                    <input name="tel_user" type="text" class='input-field' placeholder="Teléfono" required>
-                </div>
-                <div>
-                    <button class="submit-btn" type="submit" value="submit"> <span>Ingresar</span></button>
-                </div>
+            </form>
+
         </div>
     </div>
+
     <script>
         var x = document.getElementById('login');
         var y = document.getElementById('register');
