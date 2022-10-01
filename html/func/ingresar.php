5b2081e5a39e66524970cf51445f59b1982474f2
GxStyle <82037365+GxStyle@users.noreply.github.com>
2022-10-01 19:14:32 -0300

    Cerrar sesion y mantenedor
    ahora podemos cerrar la sesion y mantener datos

--- html/func/ingresar.php
+++ html/func/ingresar.php
@@ -1,17 +1,19 @@
 <?php
 require '../../database/database.php';
 
+
 $mail_usuario = $_POST['correo'];
 $clave = $_POST['clave'];
 $clave_verify ="";
 $pasa = 0;
 
-$validarUser = "SELECT mail_usuario, password FROM usuario WHERE mail_usuario='" . $_POST['correo'] . "'";
+$validarUser = "SELECT rut_usuario, mail_usuario, password FROM usuario WHERE mail_usuario='" . $_POST['correo'] . "'";
 $sqlexec = mysqli_query($connc, $validarUser);
 
 $sqlcantidad = mysqli_num_rows($sqlexec);
 if ($sqlcantidad > 0) {
     while ($dataDE = mysqli_fetch_array($sqlexec)) {
+        $rut = $dataDE['rut_usuario'];
         if (password_verify($clave, $dataDE['password'])) {
             $pasa = 1;
         }else{
@@ -24,7 +26,8 @@ if ($sqlcantidad > 0) {
 }
 
 if ($pasa == 1) {
-    echo "<script>alert('Ingreso exitoso');window.location= '../pages/login_register.php'</script>";
+    $_SESSION['usuarios_rut'] = $rut;
+    echo "<script>alert('Ingreso exitoso');window.location= '../pages/homepage.php'</script>";
 } else {
     echo "<script>alert('Error al ingresasr');window.location= '../pages/login_register.php'</script>";
 }
---
+++ html/func/mantenedor.php
@@ -0,0 +1,18 @@
+<?php
+require '../../database/database.php';
+
+if (isset($_SESSION['usuarios_rut'])) {
+    $rutUserM = $_SESSION['usuarios_rut'];
+    $sqlUserM="SELECT u.rut_usuario, u.nom_usuario, e.mail_empresa FROM usuario u INNER JOIN empresa e on u.rut_usuario = e.usuario_empresa WHERE rut_usuario = '$rutUserM'";
+    $queryUserM = mysqli_query($connc, $sqlUserM);
+
+    $dataUserM = mysqli_fetch_array($queryUserM);
+
+    $empresa= $dataUserM['mail_empresa'];
+    $rutP = $dataUserM['rut_usuario'];
+
+  }else {
+    
+  }
+
+?>
