d08812fdd6ef5d0446b3b02cc639885886d40e21
steffieess <82219313+steffieess@users.noreply.github.com>
2022-10-01 23:21:32 -0300

    Actualización
    Para emparejar je

--- html/pages/editarUsuario.php
+++ html/pages/editarUsuario.php
@@ -3,7 +3,35 @@
 <!-- header section ends -->
 
 <!--body section starts-->
-
+<section class="table-user">
+    <h1 class="heading-title"> Usuarios </h1>
+    <div class="col-md-8">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Rut</th>
+                    <th>Nombre</th>
+                    <th>Apellido Paterno</th>
+                    <th>Apellido Materno</th>
+                    <th>Correo</th>
+                    <th>Teléfono</th>
+                    <th>Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>11.111.111-2</td>
+                    <td>Victor</td>
+                    <td>Navarro</td>
+                    <td>Ruíz</td>
+                    <td>vnavarro@greentrade.cl</td>
+                    <td>+56911111111<i class="fa-solid fa-pencil"></i></td>
+                    <td>Habilitado<i class="fa-solid fa-pencil"></i></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
 <!--body section ends-->
 
 <!-- footer section starts  -->
