400fc0017ebd7c50771a5bdfdc993a35e92a316b
GxStyle <82037365+GxStyle@users.noreply.github.com>
2022-10-01 23:50:36 -0300

    Registro correcto
    ahora si

--- html/func/registrarse.php
+++ html/func/registrarse.php
@@ -36,7 +36,7 @@ if (isset($_POST['Registrar'])) {
             $queryRutUsuario = mysqli_query($connc, $queryUsuario);
             $sqlcantidad = mysqli_num_rows($queryRutUsuario);
 
-            if ($sqlcantidad < 0) {
+            if ($sqlcantidad <= 0) {
                 $queryInsertUsuario = "INSERT INTO usuario(rut_usuario, nom_usuario, ap_paterno, ap_materno, password, mail_usuario, tel_usuario, tipo_usu_id_tipousu, empresa_id_empresa) VALUES  ('$rut_user','$nom_user', '$appat_user', '$apmat_user', '$password', '$mail_user','$tel_user', 1, '$id_empresa')";
                 $resultUsuario = mysqli_query($connc, $queryInsertUsuario);
 
